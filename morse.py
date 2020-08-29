b0bb0adb0713b1c97f1b2b6abafe7876615efa26
gordomathurin <mathuringordon@gmail.com>
2020-08-28 21:53:08 -0400

    setting up frunction

--- morse.py
+++ morse.py
@@ -9,14 +9,24 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'Gordon Mathurin'
 
 from morse_dict import MORSE_2_ASCII
 
 
 def decode_bits(bits):
     # your code here
-    return
+    # maybe for loop to go thru morse dict and BITS to to compare vaule
+    # associted with BITS and then
+    # assign them to the new variable when == ????
+    vaule_match_holder = ""
+
+    for letter in bits:
+        pass
+        for vaule in MORSE_2_ASCII:
+            pass
+
+    return vaule_match_holder
 
 
 def decode_morse(morse):
@@ -26,7 +36,7 @@ def decode_morse(morse):
 
 if __name__ == '__main__':
     hey_jude_morse = ".... . -.--   .--- ..- -.. ."
-    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" # noqa
+    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"  # noqa
 
     # Be sure to run all included unit tests, not just this one.
     print("Morse Code decoder test")
